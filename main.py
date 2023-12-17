29f20fdacbc9f9bcfc1f377bac1c31bdfc726865
nata890 <natalia.ceballos28390@ucaldas.edu.co>
2023-12-16 19:53:25 -0500

    Implementación de change_password

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 from dotenv import load_dotenv
 # Carga las variables de entorno desde el archivo .env
 load_dotenv()
@@ -10,17 +11,16 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-html_file_path = "plantillas/Correo_codigo.html"
-html_content = ''
-
-# Lee el contenido del archivo HTML
-with open(html_file_path, 'r') as file:
-    html_content = file.read()
-
-
-
 @app.route('/send_email', methods=['POST'])
 def send_email():
+    
+    html_file_path = "plantillas/Correo_codigo.html"
+    html_content = ''
+
+    # Lee el contenido del archivo HTML
+    with open(html_file_path, 'r', encoding= 'utf8') as file:
+        html_content = file.read()
+
     # Obtener datos del cuerpo de la solicitud
     data = request.json
 
@@ -35,7 +35,7 @@ def send_email():
     print(f"Cuerpo: {data['body']}")
     html_content_number = html_content.replace("$$$", data['body'])
     try:
-        connection_string =os.environ.get("ACCESS_KEY")
+        connection_string =os.environ.get("CONNECTION_STRING")
         client = EmailClient.from_connection_string(connection_string)
         print(connection_string)
         message = {
@@ -55,6 +55,49 @@ def send_email():
     except Exception as ex:
         print(ex)
     return jsonify({'message': 'Email sent successfully'}), 200
+
+@app.route('/change_password', methods=['POST'])
+def change_password():
+    
+    html_file_path = "plantillas/change_password.html"
+    html_content = ''
+
+    # Lee el contenido del archivo HTML
+    with open(html_file_path, 'r', encoding= 'utf8') as file:
+        html_content = file.read()
+
+    # Obtener datos del cuerpo de la solicitud
+    data = request.json
+
+    # Simular el envío de un correo electrónico
+    print(f"Email: {data['email']}")
+
+    # Genera los 4 dígitos aleatorios para el "body"
+    random_digits = ''.join(random.choices('0123456789', k=4))
+    html_content_number = html_content.replace("$$$", random_digits)
+
+    try:
+        connection_string =os.environ.get("CONNECTION_STRING")
+        client = EmailClient.from_connection_string(connection_string)
+        print(connection_string)
+        message = {
+            "senderAddress": os.environ.get("SENDER_ADDRESS"),
+            "recipients":  {
+                "to": [{"address": data['email']}],
+            },
+            "content": {
+                "subject": "UrbanNav código",
+                "html": html_content_number
+            }
+        }
+
+        poller = client.begin_send(message)
+        result = poller.result()
+
+    except Exception as ex:
+        print(ex)
+    return jsonify({'message': 'Email sent successfully'}), 200
+
 
 if __name__ == '__main__':
     # Utiliza Waitress como servidor en lugar del servidor de desarrollo de Flask para producción

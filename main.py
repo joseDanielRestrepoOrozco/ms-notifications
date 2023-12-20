81b4db3827ea0cadee42fb9ba3af838bd90d810c
nata890 <natalia.ceballos28390@ucaldas.edu.co>
2023-12-19 19:45:56 -0500

    Implementacion de funcionalidad para enviar factura

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import random
+import pymysql
 from dotenv import load_dotenv
 from bson import DBRef, ObjectId
 # Carga las variables de entorno desde el archivo .env
@@ -23,6 +24,38 @@ def get_database():
    client = MongoClient(CONNECTION_STRING)
  
    return client['security']
+
+
+# Configuración de la conexión a la base de datos
+db_config = {
+    'host': '127.0.0.1',
+    'user': 'prog',
+    'password': 'prog',
+    'database': 'urbannav',
+    'cursorclass': pymysql.cursors.DictCursor,  #Configura el cursor para obtener resultados como diccionarios
+}
+
+# Intenta conectar a la base de datos
+try:
+    connection = pymysql.connect(**db_config)
+
+    with connection.cursor() as cursor:
+        # Ejemplo: Obtener datos de una tabla
+        query = 'SELECT * FROM vehicles;'
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+        for row in rows:
+            print(row)
+
+except Exception as e:
+    print(f'Error de conexión: {e}')
+
+finally:
+    # Cierra la conexión al finalizar
+    if 'connection' in locals() and connection.open:
+        connection.close()
+        print('Conexión cerrada')
 
 @app.route('/send_email', methods=['POST'])
 def send_email():
@@ -133,6 +166,50 @@ def change_password():
         print(ex)
     return jsonify({'message': 'Email sent successfully'}), 200
 
+@app.route('/send_bill', methods=['POST'])
+def send_bill():
+    
+    html_file_path = "plantillas/bill.html"
+    html_content = ''
+
+    # Lee el contenido del archivo HTML
+    with open(html_file_path, 'r', encoding= 'utf8') as file:
+        html_content = file.read()
+
+    # Obtener datos del cuerpo de la solicitud
+    data = request.json
+
+    # Verificar si se proporcionaron todos los campos necesarios
+    if 'email' not in data or 'subject' not in data or 'body' not in data:
+        return jsonify({'error': 'Missing required fields'}), 400
+
+    # Simular el envío de un correo electrónico (aquí puedes agregar tu lógica real de envío de correo electrónico)
+    # En este ejemplo, simplemente imprimimos la información recibida en la consola.
+    print(f"Email: {data['email']}")
+    print(f"Asunto: {data['subject']}")
+    print(f"Cuerpo: {data['body']}")
+    html_content_number = html_content.replace("###", data['body'])
+    try:
+        connection_string =os.environ.get("CONNECTION_STRING")
+        client = EmailClient.from_connection_string(connection_string)
+        print(connection_string)
+        message = {
+            "senderAddress": os.environ.get("SENDER_ADDRESS"),
+            "recipients":  {
+                "to": [{"address": data['email']}],
+            },
+            "content": {
+                "subject": data['subject'],
+                "html": html_content_number
+            }
+        }
+
+        poller = client.begin_send(message)
+        result = poller.result()
+
+    except Exception as ex:
+        print(ex)
+    return jsonify({'message': 'Email sent successfully'}), 200
 
 if __name__ == '__main__':
     # Utiliza Waitress como servidor en lugar del servidor de desarrollo de Flask para producción
